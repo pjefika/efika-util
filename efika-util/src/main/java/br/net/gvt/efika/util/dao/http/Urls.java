c96f8d5b12b4895d9fa93d0e49b231dff14df8ba
Henrique Merlin <henmerlin@gmail.com>
2018-03-05 17:33:49 -0300

    url to prod

--- efika-util/src/main/java/br/net/gvt/efika/util/dao/http/Urls.java
+++ efika-util/src/main/java/br/net/gvt/efika/util/dao/http/Urls.java
@@ -14,33 +14,34 @@ public enum Urls {
     /**
      * PROD
      */
-//    CADASTRO_STEALER("http://10.40.196.182:8080/stealerAPI/oss/"),
+//    CADASTRO_STEALER("http://10.40.198.168:8080/stealerAPI/oss/"),
 //    CADASTRO_NETWORK("http://10.200.35.67:80/networkInventoryAPI/networkInventory/"),
 //    FULLTEST("http://10.200.35.67:80/fulltestAPI/fulltest/co"),
 //    CONF_REDE("http://10.200.35.67:80/fulltestAPI/configPorta/getConfiabilidadeRede"),
 //    ONTS_DISP("http://10.200.35.67:80/fulltestAPI/configPorta/unsetOntFromOlt"),
 //    SET_ONT("http://10.200.35.67:80/fulltestAPI/configPorta/setOntToOlt"),
-//    QUEUE_COMPLETE_TASK("http://ec2-54-233-253-253.sa-east-1.compute.amazonaws.com:8080/queueAPI/queue/completeTask"),
-//    QUEUE("http://ec2-54-233-253-253.sa-east-1.compute.amazonaws.com:8080/queueAPI/queue/");
+    QUEUE_COMPLETE_TASK("http://ec2-54-233-253-253.sa-east-1.compute.amazonaws.com:8080/queueAPI/queue/completeTask"),
+    QUEUE("http://ec2-54-233-253-253.sa-east-1.compute.amazonaws.com:8080/queueAPI/queue/"),
+    QUEUE_CONSUME_TASKS("http://ec2-54-233-253-253.sa-east-1.compute.amazonaws.com:8080/queueAPI/queue/consumePendingTasks"),
     /**
      * QA
      */
-    SERVICE_INVENTORY("http://10.40.196.182:7173/stealerAPI/serviceInventory/"),
-    NETWORK_INVENTORY("http://10.40.196.182:7173/stealerAPI/networkInventory/"),
-    CADASTRO_STEALER("http://10.40.196.182:7173/stealerAPI/oss/"),
-    CADASTRO_NETWORK("http://10.40.195.81:8080/networkInventoryAPI/networkInventory/"),
-    FULLTEST("http://10.40.196.182:7172/fulltestAPI/fulltest/co"),
-    CONF_REDE("http://10.40.196.182:7172/fulltestAPI/configPorta/getConfiabilidadeRede"),
-    ONTS_DISP("http://10.40.196.182:7172/fulltestAPI/configPorta/unsetOntFromOlt"),
-    SET_ONT("http://10.40.196.182:7172/fulltestAPI/configPorta/setOntToOlt"),
-    CUSTOMERAPI_CADASTRO("http://10.40.196.182:7171/customerAPI/customer/findByParameter"),
-    CUSTOMERAPI_CERTIFICATION("http://10.40.196.182:7171/customerAPI/certification/execByParam"),
-    CUSTOMERAPI_CONF_REDE("http://10.40.196.182:7171/customerAPI/certification/confRede"),
-    CUSTOMERAPI_ONTS_DISP("http://10.40.196.182:7171/customerAPI/certification/ontsDisp"),
-    CUSTOMERAPI_SET_ONT("http://10.40.196.182:7171/customerAPI/certification/setOntToOlt"),
-    QUEUE_COMPLETE_TASK("http://10.40.196.182:7175/queueAPI/queue/completeTask"),
-    QUEUE("http://10.40.196.182:7175/queueAPI/queue/"),
-    QUEUE_CONSUME_TASKS("http://10.40.196.182:7175/queueAPI/queue/consumePendingTasks"),
+    SERVICE_INVENTORY("http://10.40.198.168:7173/stealerAPI/serviceInventory/"),
+    NETWORK_INVENTORY("http://10.40.198.168:7173/stealerAPI/networkInventory/"),
+    CADASTRO_STEALER("http://10.40.198.168:7173/stealerAPI/oss/"),
+    CADASTRO_NETWORK("http://10.200.35.67:80/networkInventoryAPI/networkInventory/"),
+    FULLTEST("http://10.40.198.168:7172/fulltestAPI/fulltest/co"),
+    CONF_REDE("http://10.40.198.168:7172/fulltestAPI/configPorta/getConfiabilidadeRede"),
+    ONTS_DISP("http://10.40.198.168:7172/fulltestAPI/configPorta/unsetOntFromOlt"),
+    SET_ONT("http://10.40.198.168:7172/fulltestAPI/configPorta/setOntToOlt"),
+    CUSTOMERAPI_CADASTRO("http://10.40.198.168:7171/customerAPI/customer/findByParameter"),
+    CUSTOMERAPI_CERTIFICATION("http://10.40.198.168:7171/customerAPI/certification/execByParam"),
+    CUSTOMERAPI_CONF_REDE("http://10.40.198.168:7171/customerAPI/certification/confRede"),
+    CUSTOMERAPI_ONTS_DISP("http://10.40.198.168:7171/customerAPI/certification/ontsDisp"),
+    CUSTOMERAPI_SET_ONT("http://10.40.198.168:7171/customerAPI/certification/setOntToOlt"),
+//    QUEUE_COMPLETE_TASK("http://10.40.198.168:7175/queueAPI/queue/completeTask"),
+//    QUEUE("http://10.40.198.168:7175/queueAPI/queue/"),
+//    QUEUE_CONSUME_TASKS("http://10.40.198.168:7175/queueAPI/queue/consumePendingTasks"),
     AUTH("http://10.40.198.168:8080/efikaAuth2/credencial/verificar");
 
     private final String valor;
