6a66a58387c4ecfef24bf4bf337cea5ee3eaadb3
Agibert <matheusagibert@gmail.com>
2018-04-30 19:07:37 -0300

    added url to method - isManageable

--- efika-util/src/main/java/br/net/gvt/efika/util/dao/http/Urls.java
+++ efika-util/src/main/java/br/net/gvt/efika/util/dao/http/Urls.java
@@ -23,6 +23,7 @@ public enum Urls {
     ONTS_DISP("http://10.40.196.171:7172/fulltestAPI/configPorta/unsetOntFromOlt"),
     SET_ONT("http://10.40.196.171:7172/fulltestAPI/configPorta/setOntToOlt"),
     GET_ONT("http://10.40.196.171:7172/fulltestAPI/configPorta/getOntFromOlt"),
+    IS_MANAGEABLE("http://10.40.196.171:7172/fulltestAPI/configPorta/isManageable"),
     CUSTOMERAPI_CADASTRO("http://10.40.196.171:7171/customerAPI/customer/findByParameter"),
     CUSTOMERAPI_CERTIFICATION("http://10.40.196.171:7171/customerAPI/certification/execByParam"),
     CUSTOMERAPI_CONF_REDE("http://10.40.196.171:7171/customerAPI/certification/confRede"),
